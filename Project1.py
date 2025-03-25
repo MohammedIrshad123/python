09daf272cdcac156656186ed6ed72e0f3718a536
MohammedIrshad123 <irshath884@gmail.com>
2025-03-25 16:06:51 +0530

    Updated the file

--- Project1.py
+++ Project1.py
@@ -25,3 +25,15 @@ a=10
 b=int('10')
 c=a+b
 print(c)
+
+#User input (while here input for a & b always string)
+a=input()
+b=input()
+c=a+b
+print(c)
+
+# Casting in user input (while here input for a & b can be casted as int)
+a=int(input())
+b=int(input())
+c=a+b
+print(c)
