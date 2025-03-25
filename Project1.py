30c2d817375746743db3a01dfa6ab8539a61daee
MohammedIrshad123 <irshath884@gmail.com>
2025-03-25 14:52:55 +0530

    new file added

---
+++ Project1.py
@@ -0,0 +1,27 @@
+# casting Probelms
+
+# integer + integer = integer
+a=10
+b=10
+c=a+b
+print(c)
+
+# string + string = string
+a="20"
+b="20"
+c=a+b
+print(c)
+
+'''
+# int + string = Type Error
+a=20
+b="20"
+e=a+b
+print(e)
+'''
+
+# int + string (convert into int) = int
+a=10
+b=int('10')
+c=a+b
+print(c)
