178cb5f80db988408e6403f7671b697d68a2c575
MohammedIrshad123 <irshath884@gmail.com>
2025-03-27 16:25:36 +0530

    new updates

--- Even.py
+++ Even.py
@@ -1,1 +1,11 @@
 # Find even number from the list or array
+
+# traditonal Method
+even=[]
+for i in range(1,100):
+    if i%2==0: 
+        print( "even number")
+        even.append(i)
+print (even)         
+
+# List comphersion
