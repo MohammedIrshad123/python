d60728bf96ec0b4eabd831d0d49ca28084506a6e
MohammedIrshad123 <irshath884@gmail.com>
2025-03-27 15:14:39 +0530

    New program

---
+++ Program1.py
@@ -0,0 +1,30 @@
+#  Number Guessing Game
+
+import random
+
+def guess_the_number():
+    number_to_guess=random.randint(1,100)
+    attempts=0
+    print("Welcome to Number guessing game")
+    print("I have choosed a number from 1 to 100, Try to guess it")
+
+    while True:
+        try:
+            guess=int(input("Enter the number to guess"))
+            attempts += 1
+
+            if guess < number_to_guess:
+                print("Too low! Try again.")
+            elif guess > number_to_guess:
+                print("Too high! Try again.")
+            else:
+                print(f"🎉 Congratulations! You guessed the number in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid number.")
+
+# Run the game
+guess_the_number()        
+            
+
+
---
+++ Program2.py
@@ -0,0 +1,16 @@
+# Let's learn about list comprehensions! 
+# You are given three integers  and  representing the dimensions of a cuboid along with an integer.
+# Print a list of all possible coordinates given by  on a 3D grid where the sum of  is not equal to. 
+# Here, Please use list comprehensions
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+# Using list comprehension to generate coordinates in list format
+coordinates = [[i, j, k] for i in range(x + 1) 
+                           for j in range(y + 1) 
+                           for k in range(z + 1) 
+                           if i + j + k != n]
+
+# Printing the output
+print(coordinates)
