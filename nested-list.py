513f92b906eff76477c46376afb6624793cd598b
MohammedIrshad123 <irshath884@gmail.com>
2025-03-28 15:47:13 +0530

    New program

---
+++ nested-list.py
@@ -0,0 +1,14 @@
+students=[]
+for x in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append ([name,score])
+        
+unique_student = sorted(set(score for name,score in students))
+
+second_lowest = unique_student[1]
+
+alphabet_order= sorted(name for name,score in students if score == second_lowest)
+
+for i in alphabet_order:
+        print("Second lowest score student"+ i)
