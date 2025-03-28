1e7d0e941a5bccf96bc6ed704105c64263c92467
MohammedIrshad123 <irshath884@gmail.com>
2025-03-28 15:11:12 +0530

    Updated Even.py

--- Even.py
+++ Even.py
@@ -1,5 +1,5 @@
 # Find even number from the list or array
-'''
+
 # traditonal Method
 even=[]
 for i in range(1,100):
@@ -7,7 +7,14 @@ for i in range(1,100):
         print( "even number")
         even.append(i)
 print (even)         
-'''
-# Using list comphersion and generator
+
+# Using list comprehensions with generator expression
 even_number=(num for num in range(1,10) if num % 2==0)
 print (list(even_number))
+
+
+# Using list comprehensions with generator expression
+even_number=(num for num in range(1,10) if num % 2==0)
+for i in even_number:
+    print (i)
+
